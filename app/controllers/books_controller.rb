72c8bc39e84b4366e3bad473220b570f48e5bb12
EC2 Default User <ec2-user@ip-172-31-10-151.ec2.internal>
2020-09-06 08:13:21 +0000

    [toukoukinou]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,12 @@
 class BooksController < ApplicationController
+  protect_from_forgery
+
   def top
   end
 
   def index
+    @book = Book
+    @books = Book.all
   end
 
   def show
@@ -13,8 +17,17 @@ class BooksController < ApplicationController
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def edit
   end
+
+  private
+  def book_params
+    params.permit(:title, :body)
+  end
 end
+
