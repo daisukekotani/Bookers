0f85f842844b1dc7109a7e63121afc643d2c846e
EC2 Default User <ec2-user@ip-172-31-10-151.ec2.internal>
2020-09-08 11:04:03 +0000

    [エラーメッセージ確認まで]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
+    @books = Book.all.order(create_at: :desc)
     @book = Book.new
   end
 
@@ -18,9 +18,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+   if  @book.save
+      flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+   else
+     @books = Book.all
+     render "index"
+   end
   end
 
   def edit
@@ -28,15 +33,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      render "edit"
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path()
+    redirect_to books_path
   end
 
   private
